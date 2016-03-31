3ad8093b4456e0db38f114aa0fbf1ea3ca7ddee6
Ara Bedrossian <arabed@yahoo.com>
2016-03-31 13:53:04 -0700

    Initial submission

---
+++ Pic2Hex/MainForm.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace Pic2Hex
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openImageFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textHexCodeOutputBox = new System.Windows.Forms.TextBox();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.panelRightBottom = new System.Windows.Forms.Panel();
+            this.buttonGenerateHexCode = new System.Windows.Forms.Button();
+            this.panelRightTop = new System.Windows.Forms.Panel();
+            this.numericUpDownY = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownX = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownContrast = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelImageTop = new System.Windows.Forms.Panel();
+            this.panelImageBottom = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panelTop.SuspendLayout();
+            this.panelBottom.SuspendLayout();
+            this.panelRight.SuspendLayout();
+            this.panelRightBottom.SuspendLayout();
+            this.panelRightTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownContrast)).BeginInit();
+            this.panelImageTop.SuspendLayout();
+            this.panelImageBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(3, 3);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 0;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "picture filename";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(672, 150);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(5, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(676, 170);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openImageFileDialog
+            // 
+            this.openImageFileDialog.Filter = "Image Files (*.bmp, *.jpg, *.png)|*.bmp;*.jpg;*.png";
+            // 
+            // textHexCodeOutputBox
+            // 
+            this.textHexCodeOutputBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textHexCodeOutputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textHexCodeOutputBox.Location = new System.Drawing.Point(5, 5);
+            this.textHexCodeOutputBox.Multiline = true;
+            this.textHexCodeOutputBox.Name = "textHexCodeOutputBox";
+            this.textHexCodeOutputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textHexCodeOutputBox.Size = new System.Drawing.Size(676, 164);
+            this.textHexCodeOutputBox.TabIndex = 4;
+            // 
+            // panelTop
+            // 
+            this.panelTop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelTop.Controls.Add(this.buttonLoad);
+            this.panelTop.Controls.Add(this.label1);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(800, 32);
+            this.panelTop.TabIndex = 7;
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.Controls.Add(this.textHexCodeOutputBox);
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom.Location = new System.Drawing.Point(0, 376);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Padding = new System.Windows.Forms.Padding(5);
+            this.panelBottom.Size = new System.Drawing.Size(686, 174);
+            this.panelBottom.TabIndex = 8;
+            // 
+            // panelRight
+            // 
+            this.panelRight.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelRight.Controls.Add(this.panelRightBottom);
+            this.panelRight.Controls.Add(this.panelRightTop);
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelRight.Location = new System.Drawing.Point(686, 32);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Padding = new System.Windows.Forms.Padding(5);
+            this.panelRight.Size = new System.Drawing.Size(114, 518);
+            this.panelRight.TabIndex = 9;
+            // 
+            // panelRightBottom
+            // 
+            this.panelRightBottom.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelRightBottom.Controls.Add(this.buttonGenerateHexCode);
+            this.panelRightBottom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRightBottom.Location = new System.Drawing.Point(5, 162);
+            this.panelRightBottom.Name = "panelRightBottom";
+            this.panelRightBottom.Padding = new System.Windows.Forms.Padding(5);
+            this.panelRightBottom.Size = new System.Drawing.Size(100, 347);
+            this.panelRightBottom.TabIndex = 13;
+            // 
+            // buttonGenerateHexCode
+            // 
+            this.buttonGenerateHexCode.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonGenerateHexCode.Location = new System.Drawing.Point(5, 298);
+            this.buttonGenerateHexCode.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonGenerateHexCode.Name = "buttonGenerateHexCode";
+            this.buttonGenerateHexCode.Padding = new System.Windows.Forms.Padding(5);
+            this.buttonGenerateHexCode.Size = new System.Drawing.Size(86, 40);
+            this.buttonGenerateHexCode.TabIndex = 7;
+            this.buttonGenerateHexCode.Text = "hex code";
+            this.buttonGenerateHexCode.UseVisualStyleBackColor = true;
+            this.buttonGenerateHexCode.Click += new System.EventHandler(this.buttonGenerateHexCode_Click);
+            // 
+            // panelRightTop
+            // 
+            this.panelRightTop.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelRightTop.Controls.Add(this.numericUpDownY);
+            this.panelRightTop.Controls.Add(this.label4);
+            this.panelRightTop.Controls.Add(this.numericUpDownX);
+            this.panelRightTop.Controls.Add(this.label3);
+            this.panelRightTop.Controls.Add(this.numericUpDownContrast);
+            this.panelRightTop.Controls.Add(this.label2);
+            this.panelRightTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelRightTop.Location = new System.Drawing.Point(5, 5);
+            this.panelRightTop.Name = "panelRightTop";
+            this.panelRightTop.Padding = new System.Windows.Forms.Padding(5);
+            this.panelRightTop.Size = new System.Drawing.Size(100, 157);
+            this.panelRightTop.TabIndex = 12;
+            // 
+            // numericUpDownY
+            // 
+            this.numericUpDownY.Dock = System.Windows.Forms.DockStyle.Top;
+            this.numericUpDownY.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownY.Location = new System.Drawing.Point(5, 84);
+            this.numericUpDownY.Margin = new System.Windows.Forms.Padding(5);
+            this.numericUpDownY.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDownY.Name = "numericUpDownY";
+            this.numericUpDownY.Size = new System.Drawing.Size(86, 20);
+            this.numericUpDownY.TabIndex = 17;
+            this.numericUpDownY.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDownY.ValueChanged += new System.EventHandler(this.numericUpDownY_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label4.Location = new System.Drawing.Point(5, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Size: Y";
+            // 
+            // numericUpDownX
+            // 
+            this.numericUpDownX.Dock = System.Windows.Forms.DockStyle.Top;
+            this.numericUpDownX.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownX.Location = new System.Drawing.Point(5, 51);
+            this.numericUpDownX.Margin = new System.Windows.Forms.Padding(5);
+            this.numericUpDownX.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDownX.Name = "numericUpDownX";
+            this.numericUpDownX.Size = new System.Drawing.Size(86, 20);
+            this.numericUpDownX.TabIndex = 13;
+            this.numericUpDownX.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDownX.ValueChanged += new System.EventHandler(this.numericUpDownX_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Location = new System.Drawing.Point(5, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Size: X";
+            // 
+            // numericUpDownContrast
+            // 
+            this.numericUpDownContrast.DecimalPlaces = 3;
+            this.numericUpDownContrast.Dock = System.Windows.Forms.DockStyle.Top;
+            this.numericUpDownContrast.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownContrast.Location = new System.Drawing.Point(5, 18);
+            this.numericUpDownContrast.Margin = new System.Windows.Forms.Padding(5);
+            this.numericUpDownContrast.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownContrast.Name = "numericUpDownContrast";
+            this.numericUpDownContrast.Size = new System.Drawing.Size(86, 20);
+            this.numericUpDownContrast.TabIndex = 12;
+            this.numericUpDownContrast.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDownContrast.ValueChanged += new System.EventHandler(this.numericUpDownContrast_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(5, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Contrast";
+            // 
+            // panelImageTop
+            // 
+            this.panelImageTop.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelImageTop.Controls.Add(this.pictureBox1);
+            this.panelImageTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelImageTop.Location = new System.Drawing.Point(0, 32);
+            this.panelImageTop.Name = "panelImageTop";
+            this.panelImageTop.Padding = new System.Windows.Forms.Padding(5);
+            this.panelImageTop.Size = new System.Drawing.Size(686, 164);
+            this.panelImageTop.TabIndex = 10;
+            // 
+            // panelImageBottom
+            // 
+            this.panelImageBottom.Controls.Add(this.pictureBox2);
+            this.panelImageBottom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelImageBottom.Location = new System.Drawing.Point(0, 196);
+            this.panelImageBottom.Name = "panelImageBottom";
+            this.panelImageBottom.Padding = new System.Windows.Forms.Padding(5);
+            this.panelImageBottom.Size = new System.Drawing.Size(686, 180);
+            this.panelImageBottom.TabIndex = 11;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 550);
+            this.Controls.Add(this.panelImageBottom);
+            this.Controls.Add(this.panelBottom);
+            this.Controls.Add(this.panelImageTop);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelTop);
+            this.Name = "MainForm";
+            this.Text = "Pic2Hex ver.0.1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.panelBottom.ResumeLayout(false);
+            this.panelBottom.PerformLayout();
+            this.panelRight.ResumeLayout(false);
+            this.panelRightBottom.ResumeLayout(false);
+            this.panelRightTop.ResumeLayout(false);
+            this.panelRightTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownContrast)).EndInit();
+            this.panelImageTop.ResumeLayout(false);
+            this.panelImageBottom.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openImageFileDialog;
+        private System.Windows.Forms.TextBox textHexCodeOutputBox;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Panel panelImageTop;
+        private System.Windows.Forms.Panel panelImageBottom;
+        private System.Windows.Forms.Panel panelRightBottom;
+        private System.Windows.Forms.Button buttonGenerateHexCode;
+        private System.Windows.Forms.Panel panelRightTop;
+        private System.Windows.Forms.NumericUpDown numericUpDownY;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownContrast;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Pic2Hex/MainForm.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pic2Hex
+{
+    public partial class MainForm : Form
+    {
+        Random rnd;
+        Image image;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            rnd = new Random((int)DateTime.Now.Ticks);
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            if (openImageFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                image = Image.FromFile(openImageFileDialog.FileName);
+                pictureBox1.Image = image;
+                pictureBox1.Height = image.Height;
+                pictureBox1.Width = image.Width;
+
+                numericUpDownX.Minimum = 0;
+                numericUpDownX.Maximum = image.Width * 2;
+                numericUpDownX.Value = image.Width;
+
+                numericUpDownY.Minimum = 0;
+                numericUpDownY.Maximum = image.Height * 2;
+                numericUpDownY.Value = image.Height;
+
+                Conversion2BW((float)numericUpDownContrast.Value);
+            }
+        }
+
+        private void Conversion2BW(float threshold)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap b1 = new Bitmap(pictureBox1.Image);
+                Bitmap b2 = new Bitmap(pictureBox1.Image);
+
+                for (int x = 0; x < b1.Size.Width; x++)
+                {
+                    for (int y = 0; y < b1.Size.Height; y++)
+                    {
+                        Color color = b1.GetPixel(x, y);
+                        float col = color.GetBrightness();
+                        Color newColor;
+                        if (col < threshold)
+                        {
+                            if (col > (threshold / 2))
+                            {
+                                newColor = rnd.Next(0, 2) == 0 ? Color.Black : Color.White;
+                            }
+                            else
+                            {
+                                newColor = Color.Black;
+                            }
+                        }
+                        else
+                        {
+                            newColor = Color.White;
+                        }
+
+                        b2.SetPixel(x, y, newColor);
+                    }
+                }
+                pictureBox2.Image = (Image)b2;
+                pictureBox2.Height = b2.Size.Height;
+                pictureBox2.Width = b2.Size.Width;
+            }
+        }
+
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            if (width <= 0 || height <= 0)
+            {
+                return null;
+            }
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        public void DoResizeImage(int width, int height)
+        {
+            Bitmap bmp = ResizeImage(image, width, height);
+            if (bmp != null)
+            {
+                pictureBox1.Image = bmp;
+                pictureBox1.Height = bmp.Height;
+                pictureBox1.Width = bmp.Width;
+                Conversion2BW((float)numericUpDownContrast.Value);
+            }
+        }
+
+        private void numericUpDownContrast_ValueChanged(object sender, EventArgs e)
+        {
+            Conversion2BW((float)numericUpDownContrast.Value);
+        }
+
+        private void buttonGenerateHexCode_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox2.Image);
+                String str_gen = "";
+                byte byt;
+                int idx = 8;
+
+                textHexCodeOutputBox.Clear();
+                textHexCodeOutputBox.AppendText("Generating code...");
+
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    byt = 0;
+                    idx = 0;
+                    for (int y = bmp.Height - 1; y >= 0; y--)
+                    {
+                        Color color = bmp.GetPixel(x, y);
+                        if (color.GetBrightness() == 0)
+                        {
+                            byt |= (byte)(1 << idx);
+                        }
+                        idx++;
+                        if (idx == 8)
+                        {
+                            str_gen += String.Format("0x{0:x2},", byt);
+                            byt = 0;
+                            idx = 0;
+                        }
+                    }
+                    str_gen += "\n";
+                }
+
+                str_gen = str_gen.Substring(0, str_gen.LastIndexOf("\n"));
+                str_gen = str_gen.Substring(0, str_gen.LastIndexOf(","));
+
+                textHexCodeOutputBox.Clear();
+                textHexCodeOutputBox.AppendText(String.Format("// Width  X:{0}\n", bmp.Width));
+                textHexCodeOutputBox.AppendText(String.Format("// Height Y:{0}\n", bmp.Height));
+                textHexCodeOutputBox.AppendText("static uint8_t hex_img[] = {\n");
+                textHexCodeOutputBox.AppendText(str_gen);
+                textHexCodeOutputBox.AppendText("\n");
+                textHexCodeOutputBox.AppendText("};\n");
+            }
+        }
+
+        private void numericUpDownX_ValueChanged(object sender, EventArgs e)
+        {
+            DoResizeImage((int)numericUpDownX.Value, (int)numericUpDownY.Value);
+        }
+
+        private void numericUpDownY_ValueChanged(object sender, EventArgs e)
+        {
+            DoResizeImage((int)numericUpDownX.Value, (int)numericUpDownY.Value);
+        }
+    }
+}
